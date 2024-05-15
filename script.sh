0f4d2d45c9b06daeb759f41c489fc2666bacb229
adilet2306 <adilet.abdraimov9@gmail.com>
2024-05-15 04:37:22 +0000

    started script

---
+++ script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+function prepare_bastion() {
+sudo apt update
+sudo apt install ansible -y
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install terraform
+}
+
+function create_instances() {
+    cd terraform
+    terraform init
+    terraform apply -auto-approve
+}
+
+
+
+prepare_bastion
